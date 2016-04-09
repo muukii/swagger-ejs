3e41ccc367c473f43cb6a860de10c14ea45f5cb8
muukii <m@muukii.me>
2016-04-09 23:37:11 +0900

    wip :D

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
 'use strict';
 
-let SwaggerParser = require('swagger-parser');
-let ejs = require('ejs');
+let SwaggerParser = require('swagger-parser')
+let ejs = require('ejs')
+let rmdir = require('rmdir')
+let mkdirp = require("mkdirp")
+let fs = require("fs")
+let getDirName = require("path").dirname
 
 let sampleSwagger = "./Swagger.yml"
+let pathTemplate = fs.readFileSync('./templates/path.ejs', 'utf-8')
+let modelTemplate = fs.readFileSync('./templates/model.ejs', 'utf-8')
 
-console.log("Validate")
-SwaggerParser.validate(sampleSwagger)
-  .then(function(api) {
-    console.log("API name: %s, Version: %s", api.info.title, api.info.version);
+let pathWritePath = "./Generated/Paths"
+let modelWritePath = "./Generated/Models"
+let fileExtension = ".swift"
+
+function writeFile (path, contents) {
+  mkdirp(getDirName(path), function (err) {
+    if (err) return
+    fs.writeFileSync(path, contents)
   })
-  .catch(function(err) {
-    console.error(err);
-  });
+}
 
-console.log("parse")
+console.log("-> Clean up...")
+
+rmdir('./Generated', (error) => {
+  console.log("-> Parse Start")
 
 SwaggerParser.parse(sampleSwagger)
-  .then(function(api) {
-    console.log(api)
-    console.log(api.paths)
+  .then((api) => {
     
-    let html = ejs.render("<%= api.info.title %>", {api:api})
-    console.log(html)
+    let info = {
+      swaggerVersion : api.swagger,
+      apiVersion : api.info.version,
+      title : api.info.title,
+      host : api.host,
+      schemes : api.schemes
+    }
+                     
+    console.log("### " + sampleSwagger + " ###")
+    
+    console.log(info)
+           
+    console.log("### Paths ###")
+    for (var path in api.paths) {
+      console.log("-" + path)
+      let pathObject = api.paths[path]
+      let pathCode = ejs.render(pathTemplate, {info: info, path: pathObject})
+      
+      let writeFilePath = pathWritePath + path + fileExtension
+      
+      writeFile(writeFilePath, pathCode)
+    }
+    
+    console.log("### Definitions ###")
+    for (var definition in api.definitions) {
+      console.log("-" + definition)      
+      let modelCode = ejs.render(modelTemplate, {info: info, path: path})
+      
+      let writeFilePath = modelWritePath + path + fileExtension
+      writeFile(writeFilePath, modelCode)
+    }
+    
   })
-  .catch(function(err) {
-    console.error(err);
-  });
+  .catch((err) => {
+    console.error(err)
+  })
+})
+
